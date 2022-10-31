602f51db7ff418909efc781faa2265f289a6cddd
FranciscoMtz117 <102782747+FranciscoMtz117@users.noreply.github.com>
2022-10-31 02:27:31 -0700

    Added GameManager, Controls menu, Changed font, Black background for main menu, and first level is chosen randomly

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameManager : MonoBehaviour
+{
+    public void StartNew()
+    {
+        // create a random int to select the first level 
+        // For sample scene change randomLevel in LoadScene() to 3
+        int randomLevel = Random.Range(2, 7);
+        SceneManager.LoadScene(randomLevel);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void ControlsMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackFromControls()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
