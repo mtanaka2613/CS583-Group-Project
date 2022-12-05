44fa19acad546f43a2e57d2bcbfe713996cf650b
FranciscoMtz117 <102782747+FranciscoMtz117@users.noreply.github.com>
2022-12-04 21:45:30 -0800

    Added Character Controller Component to Orc prefab, fixed indentation for EnemyAnimation, and replaced font for pause screen

--- Project-Files/Assets/Scripts/EnemyAnimation.cs
+++ Project-Files/Assets/Scripts/EnemyAnimation.cs
@@ -1,18 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-usingSystem.Collections;
+public class EnemyAnimation : MonoBehaviour 
+{
 
- 
 
-public class EnemyAnimation : MonoBehaviour {
-
- 
-
-         // Use this for initialization
+    // Use this for initialization
 
     private Transform player;
 
-    public float attackDistance = 2;//This is the distance to attack the target and also the target distance for pathfinding
+    public float attackDistance = 2; //This is the distance to attack the target and also the target distance for pathfinding
 
     private Animator animator;
 
@@ -24,83 +24,76 @@ public class EnemyAnimation : MonoBehaviour {
 
     private float attackCounter = 0; //timer variable
 
-         voidStart () {
+    void Start () 
+    {
 
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
-        cc = this.GetComponent<CharacterController>();
+        cc = gameObject.GetComponent<CharacterController>();
 
-        animator = this.GetComponent<Animator>();//Control the running action call in the animation state machine
+        animator = gameObject.GetComponent<Animator>(); //Control the running action call in the animation state machine
 
-        attackCounter = attackTime;//At the beginning, as long as it reaches the target, it will attack immediately
+        attackCounter = attackTime; //At the beginning, as long as it reaches the target, it will attack immediately
 
-         }
+    }
 
-        
+     // Update is called once per frame
+     void Update () 
+     {
 
-         // Update is called once per frame
+        Vector3 targetPos = player.position;
 
-         voidUpdate () {
+        targetPos.y = transform.position.y; //The role here is to assign its own Y-axis value to the target Y value
 
-        
-
-        Vector3 targetPos =player.position;
-
-        targetPos.y = transform.position.y;//The role here is to assign its own Y-axis value to the target Y value
-
-        transform.LookAt(targetPos);//When rotating, ensure that the Y axis is the axis of rotation
+        transform.LookAt(targetPos); //When rotating, ensure that the Y axis is the axis of rotation
 
         float distance = Vector3.Distance(targetPos,transform.position);
 
         if (distance <= attackDistance)
-
         {
 
             attackCounter += Time.deltaTime;
 
-            if (attackCounter >attackTime)//Realization of timer function
-
+            if (attackCounter >attackTime) //Realization of timer function
             {
 
-                int num = Random.Range(0, 2);//There are two types of attack animations, here we use random numbers ([0], [1]) to switch between the two animations
+                int num = Random.Range(0, 2); //There are two types of attack animations, here we use random numbers ([0], [1]) to switch between the two animations
 
-                if (num == 0)animator.SetTrigger("Attack01");
+                if (num == 0)
+                {
+                    animator.SetTrigger("Attack01");
+                }
+                    
 
-                else animator.SetTrigger("Attack02");
-
- 
+                else
+                {
+                    animator.SetTrigger("Attack02");
+                } 
+                    
 
                 attackCounter = 0;
-
             }
 
             else
-
             {
-
                 animator.SetBool("Walk", false);
-
- 
-
             }
-
         }
 
         else
-
         {
+            attackCounter = attackTime; //Attacks immediately every time it moves to the minimum attack distance
 
-            attackCounter = attackTime;//Attacks immediately every time it moves to the minimum attack distance
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("EnemyWalk")) //EnemyRun the state of the walk in the animation state machine
+            {  
 
-            if(animator.GetCurrentAnimatorStateInfo(0).IsName("EnemyWalk"))//EnemyRun the state of the walk in the animation state machine
+                cc.SimpleMove(transform.forward * speed);
 
-            cc.SimpleMove(transform.forward*speed);
-
-            animator.SetBool("Walk",true);//Play running animation while moving
-
+                animator.SetBool("Walk", true); //Play running animation while moving
+            }
         }
 
-         }
+     }
 
 }
 
