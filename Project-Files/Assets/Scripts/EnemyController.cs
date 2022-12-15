64f8097baa87578ff9ca94638a7bc4faa0933537
FranciscoMtz117 <102782747+FranciscoMtz117@users.noreply.github.com>
2022-12-14 19:25:35 -0800

    Restored Project-Files with backup

---
+++ Project-Files/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController: MonoBehaviour
+{
+    BoxCollider enemyCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Weapon"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Project-Files/Assets/Scripts/SceneController.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class SceneController : MonoBehaviour
+{
+    private UIActionAsset uiActionAsset;
+    [SerializeField] GameObject pauseMenu;
+
+    public TextMeshProUGUI itemsText;
+
+    private void Awake()
+    {
+        uiActionAsset = new UIActionAsset();
+        itemsText.text = "Gems: 0 / 5";
+    }
+
+    private void OnEnable()
+    {
+        uiActionAsset.UI.Pause.started += Pause;
+        uiActionAsset.UI.Enable();
+    }
+
+    private void OnDisable()
+    {
+        uiActionAsset.UI.Pause.started -= Pause;
+        uiActionAsset.UI.Disable();
+    }
+
+    public void Pause(InputAction.CallbackContext obj)
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        
+    }
+    public void StartNew()
+    {
+        GameManager.Instance.NextLevel();   
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void ControlsMenu()
+    {
+        SceneManager.LoadScene(1);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void UpdateItemsText(int gemsCount)
+    {
+        itemsText.text = "Gems: " + gemsCount + " / 5";
+    }
+
+}
